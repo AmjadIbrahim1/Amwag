21d253b7f317da7a59d439d9ef7eec05853e9324
HamadaOwner <Hamada@gmail.com>
2025-09-19 12:26:44 +0200

    feat : Initial Commit & User Feature

---
+++ services/userService.js
@@ -0,0 +1,44 @@
+import userModel from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+export const register = async ({ firstName, lastName, email, password }) => {
+  const findUser = await userModel.findOne({ email });
+  if (findUser) {
+    return { data: "User already exists", statusCode: 400 };
+  }
+  const passwordHash = await bcrypt.hash(password, 10);
+  const newUser = new userModel({
+    firstName,
+    lastName,
+    email,
+    password: passwordHash,
+  });
+  await newUser.save();
+
+  return { data: generateJWT({ firstName, lastName, email }), statusCode: 200 };
+};
+
+export const login = async ({ email, password }) => {
+  const findUser = await userModel.findOne({ email });
+  if (!findUser) {
+    return { data: "Incorrect Password or Email ", statusCode: 400 };
+  }
+  const passwordValid = await bcrypt.compare(password, findUser.password);
+
+  if (passwordValid) {
+    return {
+      data: generateJWT({
+        firstName: findUser.firstName,
+        lastName: findUser.lastName,
+        email,
+      }),
+      statusCode: 200,
+    };
+  }
+
+  return { data: "Incorrect Password or Email ", statusCode: 400 };
+};
+
+const generateJWT = (data) => {
+  return jwt.sign(data, "5adDHzTXw1owqx8MLM4btYcguKm8Eyw2");
+};
