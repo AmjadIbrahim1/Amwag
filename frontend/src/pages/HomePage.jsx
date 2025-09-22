490626d2a36d4c3787f637236e6bd73e4b5d078b
Amjad <amjadibrahim218@gmail.com>
2025-09-22 13:48:26 +0200

    feat : Static Products

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,5 +1,30 @@
-function HomePage() {
-  // return <div>Home Page</div>;
-}
+import * as React from "react";
+import CssBaseline from "@mui/material/CssBaseline";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import ProductCard from "../components/ProductCard";
+import Box from "@mui/material/Box";
 
-export default HomePage;
+export default function HomePage() {
+  return (
+    <>
+      <CssBaseline />
+      <Container maxWidth="xl" sx={{ mt: 2 }}>
+          <Grid container spacing={2} justifyContent="center" >
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+            <Grid item xs={12} sm={6} md={4} lg={3}>
+              <ProductCard />
+            </Grid>
+          </Grid>
+      </Container>
+    </>
+  );
+}
